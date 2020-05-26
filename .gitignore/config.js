19ad2c1b086b6297a46f67aef7bd1df5c3b642a8
galaxigamer <62354597+galaxigamer@users.noreply.github.com>
2020-05-26 13:50:31 +0200

    Create config.js

---
+++ .gitignore/config.js
@@ -0,0 +1,5 @@
+
+exports.TOKEN = 'NzAxNDI5NDUyNzA5NjkxNDQz.Xpxawg.aW2iTlrtFMv9iOIeHDDbZnlW_sE'; //THIS BOT IS FOR MULTIPLE GUILD
+exports.PREFIX = 'm!';
+
+exports.GOOGLE_API_KEY = 'AIzaSyADR83u3IjYzcgEHyBApnTBF6K3kNLKkFc';
